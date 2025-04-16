9f919e139fcc01a76dcbb9095172977a474db446
Amirreza Madani <madani_amirreza@yahoo.com>
2025-04-16 16:50:11 +0330

    ci(all): fix coverage

--- .tools/coverage.bat
+++ .tools/coverage.bat
@@ -12,19 +12,19 @@ del coverage\lcov.info 2> NUL
 echo NOTE sealed_annotations
 pushd sealed_annotations || exit 1
 call dart test --coverage dart_coverage || exit 1
-call dart pub global run coverage:format_coverage -i dart_coverage -o coverage/lcov.info --lcov --packages .packages --report-on lib || exit 1
+call dart pub global run coverage:format_coverage -i dart_coverage -o coverage/lcov.info --lcov --packages=.dart_tool/package_config.json --report-on lib || exit 1
 popd || exit 1
 
 echo NOTE sealed_writer
 pushd sealed_writer || exit 1
 call dart test --coverage dart_coverage || exit 1
-call dart pub global run coverage:format_coverage -i dart_coverage -o coverage/lcov.info --lcov --packages .packages --report-on lib || exit 1
+call dart pub global run coverage:format_coverage -i dart_coverage -o coverage/lcov.info --lcov --packages=.dart_tool/package_config.json --report-on lib || exit 1
 popd || exit 1
 
 echo NOTE sealed_generators
 pushd sealed_generators || exit 1
 call dart test --coverage dart_coverage || exit 1
-call dart pub global run coverage:format_coverage -i dart_coverage -o coverage/lcov.info --lcov --packages .packages --report-on lib || exit 1
+call dart pub global run coverage:format_coverage -i dart_coverage -o coverage/lcov.info --lcov --packages=.dart_tool/package_config.json --report-on lib || exit 1
 popd || exit 1
 
 echo NOTE combining coverage
