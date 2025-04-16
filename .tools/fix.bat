430075b69caa05085b051c83ad483a2ab0e021b4
Amirreza Madani <madani_amirreza@yahoo.com>
2025-04-16 10:49:39 +0330

    ci(all): fix formatting code

--- .tools/fix.bat
+++ .tools/fix.bat
@@ -4,20 +4,20 @@ echo HEADER format
 
 pushd sealed_annotations || exit 1
 echo NOTE sealed_annotations
-call dart format --fix lib test
+call dart format lib test
 popd || exit 1
 
 pushd sealed_writer || exit 1
 echo NOTE sealed_writer
-call dart format --fix lib test
+call dart format lib test
 popd || exit 1
 
 pushd sealed_generators || exit 1
 echo NOTE sealed_generators
-call dart format --fix lib test
+call dart format lib test
 popd || exit 1
 
 pushd example || exit 1
 echo NOTE example
-call dart format --fix test
+call dart format test
 popd || exit 1
